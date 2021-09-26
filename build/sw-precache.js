a76000484e92a66015d6cb5ead2f611da7349d77
XGIMI\dan.jiang <dan.jiang@xgimi.com>
2021-09-26 19:34:58 +0800

    feat(baseUrl): 处理baseurl问题 --user=ferrydjing

--- build/sw-precache.js
+++ build/sw-precache.js
@@ -2,9 +2,6 @@ module.exports = {
     cacheId: `sw-cache-nancy-tool`,
     skipWaiting: true,
     clientsClaim: true,
-    modifyURLPrefix: {
-        static: 'main-app/static'
-    },
     sourcemap: false,
     maximumFileSizeToCacheInBytes: 10000000,
     navigateFallback: '/main-app/index.html',
