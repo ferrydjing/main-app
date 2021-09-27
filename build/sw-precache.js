2b3938e074b8dfbe892e97ad2cfd11eb2e793084
XGIMI\dan.jiang <dan.jiang@xgimi.com>
2021-09-27 15:42:02 +0800

    feat(baseUrl): 处理baseurl问题 --user=ferrydjing

--- build/sw-precache.js
+++ build/sw-precache.js
@@ -5,7 +5,7 @@ module.exports = {
     sourcemap: false,
     maximumFileSizeToCacheInBytes: 10000000,
     exclude: ['/main-app/index.html'],
-    importScripts: ['/micro-app/service-worker.js'],
+    importScripts: ['/micro-app/precache-manifest.57402ca50c0f5bb67a8dec07a8eec3bf.js'],
     navigateFallback: '/main-app/index.html',
     navigateFallbackAllowlist: [/^(?!.*\.html$|\/data\/).*/]
 };
